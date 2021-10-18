99244b4c18c1b0e9ccc47e8604450f95ad2ff601
T-86 <sangokdbz@yahoo.fr>
2021-10-18 00:29:39 +0000

    Update Main.java

--- src/sanwogou/Main.java
+++ src/sanwogou/Main.java
@@ -4,8 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Tiempo");
-        System.out.println("I'm frm Togo");
-        System.out.println("Hello, my name is Mike");
+        
 
     }
 }
